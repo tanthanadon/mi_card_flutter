b0f3b7162469a2baa3cede6c4ac944d6bffec8d7
Thanadon Bunkerd <40349633+tanthanadon@users.noreply.github.com>
2020-06-03 13:24:05 +0700

    Flutter Layouts Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyAPp());
+}
+
+class MyAPp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+                child: Text('Container 1'),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('Container 3'),
+              ),
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
